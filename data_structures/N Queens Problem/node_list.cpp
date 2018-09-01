d0ced325dea3b53570ac649e118b0717c8555407
Vipul Sharma <vipuls526@gmail.com>
2018-09-02 00:50:31 +0530

    archived old files

--- data_structures/N Queens Problem/node_list.cpp
+++
@@ -1,7 +0,0 @@
-typedef struct NodeList{
-	int colnum;
-	struct NodeList* prev;
-	struct NodeList* next;
-}NList;
-typedef NList* NListptr;
-#defien NBOX (NListptr)malloc(sizeof(NList))
--- data_structures/N Queens Problem/queen_node.cpp
+++
@@ -1,9 +0,0 @@
-typedef struct queenNode{
-	int row;
-	int col;
-	struct queenNode* child;
-	struct queenNode* next;
-}QNode;
-
-typedef QNode* QNodeptr;
-#define QBOX (QNodeptr)malloc(sizeof(QNode))
