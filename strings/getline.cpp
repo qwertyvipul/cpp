e0afedee0ad96e7d1a9fe47fa4904b3a70538013
Vipul Sharma <vipuls526@gmail.com>
2018-09-03 05:26:51 +0530

    strings

---
+++ strings/getline.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+	string name;
+	cout<<"Please enter your name: ";
+	getline(cin, name);
+	cout<<"Your name is: "<<name;
+	return 0;
+}
